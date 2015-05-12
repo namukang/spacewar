7c8eaa45fa0c023730db750e86b73380a6c3dafb
Dan Kang <dan@dskang.com>
2015-05-11 21:32:04 -0400

    Detect contact

--- Spacewar/GameScene.swift
+++ Spacewar/GameScene.swift
@@ -9,11 +9,15 @@
 import SpriteKit
 import CoreMotion
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+
+    let shipCategory: UInt32 = 0x1 << 0
+    let missileCategory: UInt32 = 0x1 << 1
+    let starCategory: UInt32 = 0x1 << 2
 
     let kShipName = "ship"
     let kEnemyName = "enemy"
-    let kBulletName = "bullet"
+    let kMissileName = "missile"
     let motionManager = CMMotionManager()
 
     var tapQueue: Array<Int> = []
@@ -24,6 +28,7 @@ class GameScene: SKScene {
         scaleMode = SKSceneScaleMode.AspectFit
         physicsBody = SKPhysicsBody(edgeLoopFromRect: frame)
         physicsWorld.gravity = CGVectorMake(0, 0)
+        physicsWorld.contactDelegate = self
         backgroundColor = SKColor.blackColor()
 
         let ship = makeShip(kShipName)
@@ -39,15 +44,40 @@ class GameScene: SKScene {
         }
         addChild(enemy)
 
-        let star = SKSpriteNode(color: SKColor.whiteColor(), size: CGSizeMake(5, 5))
-        let center = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
-        star.position = center
+        let star = makeStar()
         addChild(star)
 
+        let center = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
         let gravityField = SKFieldNode.radialGravityField()
         gravityField.position = center
         gravityField.strength = 0.5
         addChild(gravityField)
+    }
+
+    func didBeginContact(contact: SKPhysicsContact) {
+        var firstBody: SKPhysicsBody, secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        if firstBody.categoryBitMask & shipCategory != 0 {
+            println("ship")
+        } else if firstBody.categoryBitMask & missileCategory != 0 {
+            println("missile")
+        } else {
+            println("star")
+        }
+
+        if secondBody.categoryBitMask & shipCategory != 0 {
+            println("ship")
+        } else if secondBody.categoryBitMask & missileCategory != 0 {
+            println("missile")
+        } else {
+            println("star")
+        }
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
@@ -56,7 +86,7 @@ class GameScene: SKScene {
         if let touch = touches.first as? UITouch {
             let location = touch.locationInView(view)
             if location.x > view?.center.x {
-                // Fire bullet
+                // Fire missile
                 tapQueue.append(1)
             } else {
                 // Propel ship
@@ -70,28 +100,24 @@ class GameScene: SKScene {
                 }
             }
         }
-
-//        for touch in (touches as! Set<UITouch>) {
-//            let location = touch.locationInNode(self)
-//            
-//            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-//            
-//            sprite.xScale = 0.5
-//            sprite.yScale = 0.5
-//            sprite.position = location
-//            
-//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-//            
-//            sprite.runAction(SKAction.repeatActionForever(action))
-//            
-//            self.addChild(sprite)
-//        }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
         processUserMotionForUpdate(currentTime)
         processUserTapsForUpdate(currentTime)
+    }
+
+    func makeStar() -> SKNode {
+        let star = SKSpriteNode(color: SKColor.whiteColor(), size: CGSizeMake(5, 5))
+        let center = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
+        star.position = center
+
+        star.physicsBody = SKPhysicsBody(circleOfRadius: 1)
+        star.physicsBody!.dynamic = false
+        star.physicsBody!.categoryBitMask = starCategory
+
+        return star
     }
 
     func makeShip(name: String) -> SKNode {
@@ -102,48 +128,52 @@ class GameScene: SKScene {
 
         ship.physicsBody = SKPhysicsBody(rectangleOfSize: ship.frame.size)
         ship.physicsBody!.mass = 1.0
+        ship.physicsBody!.categoryBitMask = shipCategory
+        ship.physicsBody!.contactTestBitMask = shipCategory | missileCategory | starCategory
+        ship.physicsBody!.collisionBitMask = 0x0
 
         return ship
     }
 
-    func makeBullet(ship: SKNode) -> SKNode {
-        let bullet = SKSpriteNode(color: SKColor.grayColor(), size: CGSizeMake(4, 8))
-        bullet.name = kBulletName
-        bullet.zRotation = ship.zRotation
+    func makeMissile(ship: SKNode) -> SKNode {
+        let missile = SKSpriteNode(color: SKColor.grayColor(), size: CGSizeMake(4, 8))
+        missile.name = kMissileName
+        missile.zRotation = ship.zRotation
 
-        bullet.physicsBody = SKPhysicsBody(rectangleOfSize: bullet.frame.size)
-        bullet.physicsBody!.velocity = ship.physicsBody!.velocity
-        bullet.physicsBody!.mass = 0.1
+        missile.physicsBody = SKPhysicsBody(rectangleOfSize: missile.frame.size)
+        missile.physicsBody!.velocity = ship.physicsBody!.velocity
+        missile.physicsBody!.mass = 0.1
+        missile.physicsBody!.categoryBitMask = missileCategory
 
-        return bullet
+        return missile
     }
 
-    func fireBullet(bullet: SKNode, destination: CGPoint, duration: CFTimeInterval) {
-        let bulletAction = SKAction.sequence([SKAction.moveTo(destination, duration: duration), SKAction.waitForDuration(3.0/60.0), SKAction.removeFromParent()])
-        bullet.runAction(bulletAction)
-        addChild(bullet)
+    func fireMissile(missile: SKNode, destination: CGPoint, duration: CFTimeInterval) {
+        let missileAction = SKAction.sequence([SKAction.moveTo(destination, duration: duration), SKAction.waitForDuration(3.0/60.0), SKAction.removeFromParent()])
+        missile.runAction(missileAction)
+        addChild(missile)
     }
 
-    func fireShipBullets() {
+    func fireShipMissiles() {
         if let ship = childNodeWithName(kShipName) {
-            let bullet = makeBullet(ship)
+            let missile = makeMissile(ship)
 
             let shipDirection = Float(ship.zRotation) + Float(M_PI_2)
-            let padding = ship.frame.size.height - bullet.frame.size.height / 2
-            let bulletX = ship.position.x + CGFloat(cosf(shipDirection)) * padding
-            let bulletY = ship.position.y + CGFloat(sinf(shipDirection)) * padding
-            bullet.position = CGPointMake(bulletX, bulletY)
+            let padding = ship.frame.size.height - missile.frame.size.height / 2
+            let missileX = ship.position.x + CGFloat(cosf(shipDirection)) * padding
+            let missileY = ship.position.y + CGFloat(sinf(shipDirection)) * padding
+            missile.position = CGPointMake(missileX, missileY)
 
             let destX = ship.position.x + CGFloat(cosf(shipDirection)) * 500
             let destY = ship.position.y + CGFloat(sinf(shipDirection)) * 500
-            let bulletDestination = CGPointMake(destX, destY)
-            fireBullet(bullet, destination: bulletDestination, duration: 1.0)
+            let missileDestination = CGPointMake(destX, destY)
+            fireMissile(missile, destination: missileDestination, duration: 1.0)
         }
     }
 
     func processUserTapsForUpdate(currentTime: CFTimeInterval) {
         for tap in tapQueue {
-            fireShipBullets()
+            fireShipMissiles()
             tapQueue.removeAtIndex(0)
         }
     }
